0606a489ff7dd2fd393870288816997a47b906fa
RaleGale <fon.mobitel@gmail.com>
2023-11-28 16:58:48 +0100

    Update dump_efuse.sh

--- scripts/s905x-h/dump_efuse.sh
+++ scripts/s905x-h/dump_efuse.sh
@@ -15,7 +15,22 @@ cd dump_s905x-h
 
 #./bin/update identify 7
 
-echo "Dump Efuse 0xD9000000"
+echo "Dump SRAM 0xD9000000@14000"
 
 ../bin/amlogic-usbdl s905x-h ../payloads/s905x-h/bin/memdump_efuse.bin s905x-h_efuse.bin
 
+echo "Dump Efuse"
+
+dd if=s905x-h_efuse.bin bs=1 skip=80896 count=255 of=efuse.bin status=none
+
+echo "Dump Password + Salt"
+
+dd if=s905x-h_efuse.bin bs=1 skip=62728 count=20 of=password_salt.bin status=none
+
+echo "Dump Password"
+
+dd if=s905x-h_efuse.bin bs=1 skip=62728 count=16 of=password.bin status=none
+
+echo "Dump AESKEY"
+
+dd if=efuse.bin bs=1 skip=80 count=32 of=aeskey.bin status=none
